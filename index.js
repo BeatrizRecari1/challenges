655cf1caa0dd5f6956c6b78556fce977b95a2020
Beatriz Recari <beatrizrecari@gmail.com>
2023-03-16 21:28:27 -0400

    Added another challenge to the list

---
+++ index.js
@@ -0,0 +1,133 @@
+// //
+
+// function isLargestSwap(num) {
+//   const tens = num / 10;
+//   const ones = num % 10;
+//   return tens >= ones;
+// }
+
+// console.log(isLargestSwap(26)); // Expected output: false
+// console.log(isLargestSwap(74)); // Expected output: true
+
+// // Write a fuction that, given any interger greater than zero, returns the factorial of that number. i.e. factorial(5) = 5!= 5*4*3*2*1
+
+// function factorial_1(num) {
+//   let result = 1;
+//   for (let i = 0; i <= num; i++) {
+//     newNumber *= i;
+//   }
+//   return result;
+// }
+
+// console.log(factorial_1(5));
+
+// // Another way to write it
+
+// async function factorial_2(num) {
+//   if (num === 0) return 1;
+//   const result = await factorial_2(num - 1);
+//   return num * factorial_2(num - 1);
+// }
+
+// async function testTimes() {
+//   console.time("factorial_1");
+//   const ans1 = await factorial_1(500);
+//   console.log(ans1);
+//   console.timeEnd("factorial_1");
+//   console.time("factorial_2");
+//   const ans2 = await factorial_2(500);
+//   console.log(ans2);
+//   console.timeEnd;
+//   console.time;
+// }
+// console.log(factorial_1(5));
+// console.log(factorial_2(5));
+
+// // Write a function that takes in an array of numbers and returns the third largest number.
+
+// function thirdLargestNumber(arr) {
+//   let largestThree = [0, 0, 0];
+//   for (let i = 0; i < num.length; i++) {
+//     if (arr[i] > largestThree[0]) {
+//       largestThree.splice(0, 0, arr[i]);
+//     } else if (arr[i] > largestThree[1]) {
+//       largestThree.splice(1, 0, arr[i]);
+//     } else if (arr[i] > largestThree[2]) {
+//       largestThree.splice(2, 0, arr[i]);
+//     }
+//     console.table({ i, elem: arr[i], largestThree });
+//   }
+//   return largestThree[2];
+// }
+
+// console.log(thirdLargestNumber([5, 4, 3, 2, 7, 8, 9, 1, 10, 13]));
+
+// // Another way to write it without many methods
+
+// function getThirdLargest(arr) {
+//   let largest = 0,
+//     secondLargest = 0,
+//     thirdLargest = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].largest) {
+//       thirdLargest = secondLargest;
+//       secondLargest = largest;
+//       largest = arr[i];
+//     } else if (arr[i] > secondLargest) {
+//       thirdLargest = secondLargest;
+//       secondLargest = arr[i];
+//     } else if (arr[i] > thirdLargest) {
+//       thirdLargest = arr[i];
+//     }
+//   }
+//   return thirdLargest;
+// }
+
+// console.log(getThirdLargest([5, 6, 3, 2, 7, 8, 1, 10, 11]));
+
+// // Another way
+
+// function thirdLargestNumber(arr) {
+//   // Sort the array in descending order
+//   arr.sort(function (a, b) {
+//     return b - a;
+//   });
+
+//   // Return the third element in the sorted array
+//   return arr[2];
+// }
+
+// Write a function that takes in two sorted arrays of numbers and returns a single sorted array containing all elements from the given arrays.
+const arr1 = [1, 3, 5, 7];
+const arr2 = [2, 4, 5, 6];
+
+function sortedBigArray(arr1, arr2) {
+  const mixedArray = [...arr1, ...arr2];
+  //mixedArray = [1,3,5,7,2,4,5,6]
+  let newArray = [];
+  for (let i = 0; i < mixedArray.length; i++) {
+    if (mixedArray[i] > newArray[i]) {
+      newArray += mixedArray[i];
+    }
+    if (mixedArray[i] < newArray[i]) {
+      for (let j = 0; j < newArray.length; j++) {
+        if (newArray[j] < mixedArray[i]) {
+          newArray += mixedArray[i];
+        }
+      }
+    }
+    if (mixedArray[i] === mixedArray[i + 1]) {
+      newArray += mixedArray[i + 1];
+    }
+  }
+  return newArray;
+}
+
+console.log(sortedBigArray(arr1, arr2));
+
+// Create a function "HigestDigit" that takes a number as an argument and returns the highest digit in that number
+
+const highestDigit = (number) => {
+  number = number.toString();
+  return Math.max(...number);
+};
