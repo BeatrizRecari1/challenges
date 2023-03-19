230d8c99c261ab09f5a57c3bfe6bc4d5efacdbc5
Beatriz Recari <beatrizrecari@gmail.com>
2023-03-19 13:25:00 -0700

    Added a my daily code challenge from Sonny Sangha

--- index.js
+++ index.js
@@ -140,9 +140,22 @@ const uncensor = (str, vowels) => {
   return str.replace(/\*/g, () => arr.shift());
 };
 
+// Alternate solution
+
 function uncensor(str, vowels) {
   for (let i = 0; i < vowels.length; i++) {
     str = str.replace("*", vowels(i));
   }
   return str;
 }
+
+// You will be given two extremely similar arrays, but exactly one of the items in an array will be valued slightly higher than its counter part (which means that evaluating the value > the other value will return true)
+// Create a function isSuperior that returns whether the first array is slightly superior to that of the second array
+
+function isSuperior(arr1, arr2) {
+  return arr1 > arr2;
+}
+
+// Alternate solution
+
+const isSuperior = (arr1, arr2) => arr1 > arr2;
