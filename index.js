5b97c4b316f0af50e6aec59c2c586ff5d6c2deba
Beatriz Recari <beatrizrecari@gmail.com>
2023-03-24 07:44:05 -0700

    added daily code challenge

--- index.js
+++ index.js
@@ -203,3 +203,9 @@ function multiSum(num, ten = 0) {
 // Alternate solution
 
 const multiSum = (n) => n * 55;
+
+// Create a function "derivative" that takes numbers b and m as arguments and returns the derivative of the function f(x)=x^b with respect to x evaluated at x=m, where b and m are constants.
+
+function derivative(b, m) {
+  return b * m ** (b - 1);
+}
