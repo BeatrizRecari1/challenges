e4bd2f30e1f51d65cea036784da1c103e24812b6
Beatriz Recari <beatrizrecari@gmail.com>
2023-03-27 20:21:55 -0400

    added second code challenge LeetCode of the day

--- index.js
+++ index.js
@@ -282,3 +282,55 @@ const runningSum = function (nums) {
   }
   return nums;
 };
+
+// Given an array of integers nums, calculate the pivot index of this array.
+
+// The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+// If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.// Return the leftmost pivot index. If no such index exists, return -1.
+
+const pivotIndex = (nums) => {
+  // We have not started our program yet, so values are at 0
+  let rightSum = 0;
+  let leftSum = 0;
+
+  // Calculate the sum of the entire array
+  // O(n)
+  nums.forEach((v) => (rightSum += v));
+
+  // O(n)
+  for (var i = 0; i <= nums.length - 1; i++) {
+    let curr = nums[i];
+
+    // The right sum no longer contains the curr number
+    rightSum -= curr;
+
+    // if left sum equals right sum, we return index
+    if (leftSum === rightSum) return i;
+
+    // We did not find a match
+    // Left sum now contains our curr number
+    leftSum += curr;
+  }
+
+  return -1;
+};
+
+// Alternate solution
+function pivotIndex(nums) {
+  const totalSum = nums.reduce((sum, num) => sum + num, 0);
+  // Initialize a variable to store the left sum
+  let leftSum = 0;
+  // Iterate over the elements in the array
+  for (let i = 0; i < nums.length; i++) {
+    // If the sum of the numbers to the left of the index is equal to the sum of the numbers to the right of the index
+    if (leftSum === totalSum - leftSum - nums[i]) {
+      // Return the pivot index
+      return i;
+    }
+    // Add the current element to the left sum
+    leftSum += nums[i];
+  }
+  // If no pivot index is found, return -1
+  return -1;
+}
