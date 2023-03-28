283e344f4d0fb217c7c99488c73b51a2b807a90e
Beatriz Recari <beatrizrecari@gmail.com>
2023-03-28 16:32:12 -0400

    added daily code challenge from LeetCode

--- index.js
+++ index.js
@@ -334,3 +334,26 @@ function pivotIndex(nums) {
   // If no pivot index is found, return -1
   return -1;
 }
+
+// Given two strings s and t, determine if they are isomorphic.
+// Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+// All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+// Runtime: 83 ms, faster than 88.18% of JavaScript online submissions for Isomorphic Strings.
+// Time Complexity : O(n)
+var isIsomorphic = function (s, t) {
+  // Base case: for different length of two strings...
+  if (s.length != t.length) return false;
+  // Create two maps for s & t strings...
+  const map1 = [256];
+  const map2 = [256];
+  // Traverse all elements through the loop...
+  for (let i = 0; i < s.length; i++) {
+    // Compare the maps, if not equal, return false...
+    if (map1[s.charAt(i)] != map2[t.charAt(i)]) return false;
+    // Insert each character if string s and t into seperate map...
+    map1[s.charAt(i)] = i + 1;
+    map2[t.charAt(i)] = i + 1;
+  }
+  return true; // Otherwise return true...
+};
