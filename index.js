3dfe6fff0c864632cee178a500bcd87fc9fa0628
Beatriz Recari <beatrizrecari@gmail.com>
2023-03-29 20:47:22 -0400

    added daily code challenge from LeetCode

--- index.js
+++ index.js
@@ -373,3 +373,36 @@ function isSubsequence(s, t) {
   }
   return i === s.length;
 }
+
+// You are given the heads of two sorted linked lists list1 and list2.
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+// Return the head of the merged linked list.
+
+function mergeTwoLists(list1, list2) {
+  // Create a dummy node to serve as the head of the merged list
+  const dummy = new ListNode(0);
+  let current = dummy;
+
+  // Loop through both input lists, comparing their values and adding the smallest node to the merged list
+  while (list1 !== null && list2 !== null) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  // Add any remaining nodes from list1 or list2 to the end of the merged list
+  if (list1 !== null) {
+    current.next = list1;
+  }
+  if (list2 !== null) {
+    current.next = list2;
+  }
+
+  // Return the head of the merged list (ignoring the dummy node)
+  return dummy.next;
+}
