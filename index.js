22fc56cc776b30f113d4c310e006733f3f4a2f7c
Beatriz Recari <beatrizrecari@gmail.com>
2023-03-30 16:22:42 -0400

    added second daily code challenge from LeetCode

--- index.js
+++ index.js
@@ -437,3 +437,32 @@ function findMiddleNode(head) {
 
   return slow;
 }
+
+// Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+// Do not modify the linked list.
+
+function detectCycle(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    // If there is a cycle, move two pointers at the same speed
+    if (slow === fast) {
+      // Reset slow to head and move both pointers at the same speed
+      slow = head;
+      while (slow !== fast) {
+        slow = slow.next;
+        fast = fast.next;
+      }
+      // The point at which both pointers meet is the start of the cycle
+      return slow;
+    }
+  }
+
+  // If we get here, there is no cycle
+  return null;
+}
