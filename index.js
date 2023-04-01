4b5635e20f9bc3a4588508dfc6ad6a24c4f88e1e
Beatriz Recari <beatrizrecari@gmail.com>
2023-03-31 23:00:58 -0400

    added daily code challenge from LeetCode

--- index.js
+++ index.js
@@ -466,3 +466,28 @@ function detectCycle(head) {
   // If we get here, there is no cycle
   return null;
 }
+
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+function maxProfit(prices) {
+  if (!prices || prices.length === 0) {
+    return 0;
+  }
+
+  let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    } else if (prices[i] - minPrice > maxProfit) {
+      maxProfit = prices[i] - minPrice;
+    }
+  }
+
+  return maxProfit;
+}
