4c71d27d79ea3ccac7be788f6c472d6e500ed1ae
Beatriz Recari <beatrizrecari@gmail.com>
2023-04-02 17:21:32 -0400

    added daily code challenge from LeetCode

--- index.js
+++ index.js
@@ -572,3 +572,46 @@ function levelOrder(root) {
 
   return result;
 }
+
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+// You must write an algorithm with O(log n) runtime complexity.
+
+function search(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+// You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+// Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+// You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+function firstBadVersion(n) {
+  let left = 1;
+  let right = n;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (isBadVersion(mid)) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
+}
