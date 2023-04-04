be934eee5498b042dda7cd859c2d27bbb5ebb373
Beatriz Recari <beatrizrecari@gmail.com>
2023-04-03 20:34:53 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -640,3 +640,16 @@ function isValidBST(root) {
 
   return isValid(root, null, null);
 }
+
+// Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+// According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+function lowestCommonAncestor(root, p, q) {
+  if (root.val > p.val && root.val > q.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  } else if (root.val < p.val && root.val < q.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  } else {
+    return root;
+  }
+}
