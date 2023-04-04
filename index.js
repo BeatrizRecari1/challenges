e672257c12b3f429c4a731537904264e86e51b3c
Beatriz Recari <beatrizrecari@gmail.com>
2023-04-03 20:33:21 -0400

    added daily code challenge from LeetCode

--- index.js
+++ index.js
@@ -615,3 +615,28 @@ function firstBadVersion(n) {
 
   return left;
 }
+
+// Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+// A valid BST is defined as follows:
+// The left subtree of a node contains only nodes with keys less than the node's key.
+// The right subtree of a node contains only nodes with keys greater than the node's key.
+// Both the left and right subtrees must also be binary search trees.
+
+function isValidBST(root) {
+  function isValid(node, min, max) {
+    if (!node) {
+      return true; // an empty tree is a valid BST
+    }
+    if (
+      (min !== null && node.val <= min) ||
+      (max !== null && node.val >= max)
+    ) {
+      return false; // violates the BST property
+    }
+    return (
+      isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
+    );
+  }
+
+  return isValid(root, null, null);
+}
