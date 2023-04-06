1c4c4783d5299e2a10f529d7faf59eb1dbd9fe95
Beatriz Recari <beatrizrecari@gmail.com>
2023-04-05 20:43:13 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -653,3 +653,35 @@ function lowestCommonAncestor(root, p, q) {
     return root;
   }
 }
+
+// An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+// You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+// To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with color.
+// Return the modified image after performing the flood fill.
+
+function floodFill(image, sr, sc, color) {
+  const oldColor = image[sr][sc];
+
+  // If the starting pixel is already of the target color, no need to fill
+  if (oldColor === color) {
+    return image;
+  }
+
+  const dfs = (r, c) => {
+    // Check if current pixel is of oldColor
+    if (image[r][c] === oldColor) {
+      image[r][c] = color;
+
+      // Recursively fill adjacent pixels
+      if (r > 0) dfs(r - 1, c);
+      if (c > 0) dfs(r, c - 1);
+      if (r < image.length - 1) dfs(r + 1, c);
+      if (c < image[0].length - 1) dfs(r, c + 1);
+    }
+  };
+
+  // Start the flood fill from the starting pixel
+  dfs(sr, sc);
+
+  return image;
+}
