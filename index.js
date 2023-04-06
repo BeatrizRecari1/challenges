352a1a805422c9cb052c46800455af8676ab7708
Beatriz Recari <beatrizrecari@gmail.com>
2023-04-05 20:44:34 -0400

    added second daily challenge

--- index.js
+++ index.js
@@ -685,3 +685,39 @@ function floodFill(image, sr, sc, color) {
 
   return image;
 }
+
+// Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+// An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+function numIslands(grid) {
+  const dfs = (i, j) => {
+    if (
+      i < 0 ||
+      i >= grid.length ||
+      j < 0 ||
+      j >= grid[0].length ||
+      grid[i][j] !== "1"
+    ) {
+      return;
+    }
+    grid[i][j] = "0"; // mark the current land as visited
+    dfs(i + 1, j); // search down
+    dfs(i - 1, j); // search up
+    dfs(i, j + 1); // search right
+    dfs(i, j - 1); // search left
+  };
+
+  let islandCount = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === "1") {
+        // if current cell is land
+        dfs(i, j); // search for adjacent lands
+        islandCount++; // increment island count
+      }
+    }
+  }
+
+  return islandCount;
+}
