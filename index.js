06a83aa15488bc9f2b4309cbb213a8ad0e653f5b
Beatriz Recari <beatrizrecari@gmail.com>
2023-04-06 20:39:22 -0400

    added daily code challenge from LeetCode

--- index.js
+++ index.js
@@ -721,3 +721,13 @@ function numIslands(grid) {
 
   return islandCount;
 }
+
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1.
+// Given n, calculate F(n).
+
+const fib = (n, res = [0, 1]) =>
+  n == 0 || n == 1 || res[n]
+    ? res[n]
+    : (res[n] = fib(n - 1, res) + fib(n - 2, res));
