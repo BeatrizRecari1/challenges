0cf6713643c32f73fe17d809da10c2a6b8a10851
Beatriz Recari <beatrizrecari@gmail.com>
2023-04-06 20:43:03 -0400

    added second daily code challenge

--- index.js
+++ index.js
@@ -731,3 +731,18 @@ const fib = (n, res = [0, 1]) =>
   n == 0 || n == 1 || res[n]
     ? res[n]
     : (res[n] = fib(n - 1, res) + fib(n - 2, res));
+
+// You are climbing a staircase. It takes n steps to reach the top.
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  let arr = [1, 2];
+  for (let i = 2; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr[n - 1];
+};
