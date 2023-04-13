1a8f4bae0a38d4ad02179e3d9eb3724699f3a25e
Beatriz Recari <beatrizrecari@gmail.com>
2023-04-12 19:59:46 -0700

    added daily code challenge

--- index.js
+++ index.js
@@ -877,3 +877,45 @@ function isHappy(n) {
   }
   return n === 1; // if n is 1, it's a happy number; otherwise, it's not
 }
+
+// Given an m x n matrix, return all elements of the matrix in spiral order.
+
+function spiralOrder(matrix) {
+  const result = [];
+  let rowStart = 0;
+  let rowEnd = matrix.length - 1;
+  let colStart = 0;
+  let colEnd = matrix[0].length - 1;
+
+  while (rowStart <= rowEnd && colStart <= colEnd) {
+    // Traverse right
+    for (let i = colStart; i <= colEnd; i++) {
+      result.push(matrix[rowStart][i]);
+    }
+    rowStart++;
+
+    // Traverse down
+    for (let i = rowStart; i <= rowEnd; i++) {
+      result.push(matrix[i][colEnd]);
+    }
+    colEnd--;
+
+    // Traverse left
+    if (rowStart <= rowEnd) {
+      for (let i = colEnd; i >= colStart; i--) {
+        result.push(matrix[rowEnd][i]);
+      }
+      rowEnd--;
+    }
+
+    // Traverse up
+    if (colStart <= colEnd) {
+      for (let i = rowEnd; i >= rowStart; i--) {
+        result.push(matrix[i][colStart]);
+      }
+      colStart++;
+    }
+  }
+
+  return result;
+}
