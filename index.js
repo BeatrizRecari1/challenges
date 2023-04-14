dc001183a091f0fededbcfeb30f38cf3eb953bdc
Beatriz Recari <beatrizrecari@gmail.com>
2023-04-14 15:28:04 -0700

    added second daily code challenge

--- index.js
+++ index.js
@@ -941,3 +941,13 @@ function greet(name, owner) {
     return "Hello guest";
   }
 }
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+function newString(n, s) {
+  let str = "";
+  for (let i = 0; i < n; i++) {
+    str += s;
+  }
+  return str;
+}
