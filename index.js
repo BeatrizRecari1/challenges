2c20ec07c3bff5b4b38a68081d8d594a47fec8b1
Beatriz Recari <beatrizrecari@gmail.com>
2023-04-14 15:16:21 -0700

    added daily code challenge

--- index.js
+++ index.js
@@ -930,3 +930,14 @@ function paperwork(n, m) {
     return n * m;
   }
 }
+
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+function greet(name, owner) {
+  // Add code here
+  if (name === owner) {
+    return "Hello boss";
+  } else {
+    return "Hello guest";
+  }
+}
