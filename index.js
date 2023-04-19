6693d7e9f8fa64052d12003288e1e242de05aa2e
Beatriz Recari <beatrizrecari@gmail.com>
2023-04-19 10:01:06 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -986,3 +986,13 @@ function getGrade(s1, s2, s3) {
     return "F";
   }
 }
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord(bool) {
+  if (bool === true) {
+    return "Yes";
+  } else {
+    return "No";
+  }
+}
