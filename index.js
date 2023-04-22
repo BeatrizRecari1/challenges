c4cf0e87508fb02c75e94854588e90a5abc0e24c
Beatriz Recari <beatrizrecari@gmail.com>
2023-04-22 17:18:26 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -1006,3 +1006,10 @@ function smashWordsIntoSentence(words) {
 
   return words.join(" ");
 }
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+function greet(name) {
+  //your code here
+  return `Hello, ${name} how are you doing today?`;
+}
