f8f2982f4d6bd0fcfe1a0b7ab03480850d9fb07e
Beatriz Recari <beatrizrecari@gmail.com>
2023-04-23 16:24:59 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -1013,3 +1013,13 @@ function greet(name) {
   //your code here
   return `Hello, ${name} how are you doing today?`;
 }
+
+// The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+function cockroachSpeed(s) {
+  const speedCmPs = (s * 100000) / 3600;
+
+  const roundedSpeed = Math.floor(speedCmPs);
+
+  return roundedSpeed;
+}
