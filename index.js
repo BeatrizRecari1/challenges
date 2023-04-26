944efe293c1dc4fe16b429d13102ed791bbfb068
Beatriz Recari <beatrizrecari@gmail.com>
2023-04-26 17:17:02 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -1056,3 +1056,23 @@ function inLove(flower1, flower2) {
     return false;
   }
 }
+
+// Let's play! You have to return which player won! In case of a draw return Draw!. Rock-Paper-Scissors
+
+const rps = (p1, p2) => {
+  if (p1 === "scissors" && p2 === "paper") {
+    return "Player 1 won!";
+  } else if (p1 === "rock" && p2 === "scissors") {
+    return "Player 1 won!";
+  } else if (p1 === "paper" && p2 === "rock") {
+    return "Player 1 won!";
+  } else if (p1 === "scissors" && p2 === "rock") {
+    return "Player 2 won!";
+  } else if (p1 === "paper" && p2 === "scissors") {
+    return "Player 2 won!";
+  } else if (p1 === "rock" && p2 === "paper") {
+    return "Player 2 won!";
+  } else {
+    return "Draw!";
+  }
+};
