066e5a239e94629f9283356d3c9bc0ad59da04ce
Beatriz Recari <beatrizrecari@gmail.com>
2023-04-27 21:37:23 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -1076,3 +1076,9 @@ const rps = (p1, p2) => {
     return "Draw!";
   }
 };
+
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+function digitize(n) {
+  return Array.from(String(n), Number).reverse();
+}
