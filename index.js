9b353ceee2ec467961edd5686a05ab2969de5ab3
Beatriz Recari <beatrizrecari@gmail.com>
2023-05-04 20:40:08 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -1174,3 +1174,14 @@ function nbYear(p0, percent, aug, p) {
   }
   return years;
 }
+
+
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+function findTheNeedle(haystack) {
+  return "found the needle at position " + haystack.indexOf("needle")
+}
