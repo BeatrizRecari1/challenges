7c6f98452388787a94eafc425254d44bd906a09d
Beatriz Recari <beatrizrecari@gmail.com>
2023-05-05 21:05:19 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -1185,3 +1185,25 @@ function nbYear(p0, percent, aug, p) {
 function findTheNeedle(haystack) {
   return "found the needle at position " + haystack.indexOf("needle")
 }
+
+
+
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+function sumArray(array) {
+  if (array == null || array.length <= 2) {
+      return 0
+    }
+    
+    var max = Math.max.apply(Math, array);
+    var min = Math.min.apply(Math, array);
+    var sum = 0
+    
+    for (i = 0; i < array.length; i++) {
+      sum += array[i];
+     }
+  
+    return sum - max - min
+  }
