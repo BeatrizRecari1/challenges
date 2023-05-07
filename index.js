ca042db7f2f98a232835cc923b78b7d7561809d6
Beatriz Recari <beatrizrecari@gmail.com>
2023-05-07 14:10:07 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -1235,3 +1235,9 @@ function countPositivesSumNegatives(input) {
 
   return newArr;
 }
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+function solution(str, ending) {
+  return str.endsWith(ending);
+}
