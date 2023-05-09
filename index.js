1e24ecd1e15d19645649f56eb1230c03ac27c694
Beatriz Recari <beatrizrecari@gmail.com>
2023-05-09 21:42:53 +0200

    added daily code challenge

--- index.js
+++ index.js
@@ -1274,3 +1274,13 @@ function arrayWithValue(a, x) {
 function arrayWithValue(a, x) {
   return a.includes(x);
 }
+
+// Trolls are attacking your comment section!
+
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+function disenvowel(str) {
+  return str.replace(/[aeiou]/gi, "");
+}
