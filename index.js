4e3a816ac50d0207ed3ff998c012d2a4dae407f2
Beatriz Recari <beatrizrecari@gmail.com>
2023-05-21 17:25:40 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -1323,3 +1323,10 @@ function getCount(str) {
 
   return vowelsCount;
 }
+
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+function addBinary(a, b) {
+  const sum = a + b;
+  return sum.toString(2);
+}
