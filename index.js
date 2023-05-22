7c4146aac02eb683811e22d75718b281b57c8abc
Beatriz Recari <beatrizrecari@gmail.com>
2023-05-22 21:45:00 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -1330,3 +1330,15 @@ function addBinary(a, b) {
   const sum = a + b;
   return sum.toString(2);
 }
+
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+function countSheeps(arrayOfSheep) {
+  let sum = 0;
+  for (let i = 0; i < arrayOfSheep.length; i++) {
+    if (arrayOfSheep[i] === true) {
+      sum += arrayOfSheep[i];
+    }
+  }
+  return sum;
+}
