4281539f190c46fc7ccf8120553c202aeb4b5ae3
Beatriz Recari <beatrizrecari@gmail.com>
2023-05-25 17:02:49 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -1342,3 +1342,23 @@ function countSheeps(arrayOfSheep) {
   }
   return sum;
 }
+
+// Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+var min = function (list) {
+  var min = list[0];
+  for (var i = 0; i < list.length; i++) {
+    var cur = list[i];
+    if (cur < min) min = cur;
+  }
+  return min;
+};
+
+var max = function (list) {
+  var max = list[0];
+  for (var i = 0; i < list.length; i++) {
+    var cur = list[i];
+    if (cur > max) max = cur;
+  }
+  return max;
+};
