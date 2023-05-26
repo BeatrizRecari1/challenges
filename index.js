7f2e5e9ed9d8cea348a184bb3ed1e86ceb759e4d
Beatriz Recari <beatrizrecari@gmail.com>
2023-05-26 17:04:05 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -1362,3 +1362,13 @@ var max = function (list) {
   }
   return max;
 };
+
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+var number = function (array) {
+  return array.map(function (line, index) {
+    return index + 1 + ": " + line;
+  });
+};
