ee4c6df3178b4ebc814758c92aedcd090d49e26b
Beatriz Recari <beatrizrecari@gmail.com>
2023-05-30 17:14:49 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -1404,3 +1404,17 @@ function FirstFactorial(num) {
     return factorial;
   }
 }
+
+// Have the function CheckNums(num1,num2) take both parameters being passed and return the string true if num2 is greater than num1, otherwise return the string false. If the parameter values are equal to each other then return the string -1.
+
+function CheckNums(num1, num2) {
+  // code goes here
+  if (num1 > num2) {
+    return "false";
+  } else if (num1 < num2) {
+    return "true";
+  } else {
+    return "-1";
+  }
+  return num1;
+}
