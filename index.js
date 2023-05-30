1f731aa93df911cc72d5629e3352fc27e55e898e
Beatriz Recari <beatrizrecari@gmail.com>
2023-05-30 17:19:59 +0100

    added second daily code challenge

--- index.js
+++ index.js
@@ -1418,3 +1418,14 @@ function CheckNums(num1, num2) {
   }
   return num1;
 }
+
+// Have the function SimpleAdding(num) add up all the numbers from 1 to num. For example: if the input is 4 then your program should return 10 because 1 + 2 + 3 + 4 = 10. For the test cases, the parameter num will be any number from 1 to 1000.
+
+function SimpleAdding(num) {
+  // code goes here
+  let sum = 0;
+  for (let i = 0; i < num; i++) {
+    sum += i;
+  }
+  return sum;
+}
