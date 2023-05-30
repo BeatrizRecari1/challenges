f67e9f6471d0f712dbc935d3b601cfe26eac5c28
Beatriz Recari <beatrizrecari@gmail.com>
2023-05-30 17:32:36 +0100

    added third daily code challenge

--- index.js
+++ index.js
@@ -1429,3 +1429,16 @@ function SimpleAdding(num) {
   }
   return sum;
 }
+
+// Have the function SimpleEvens(num) check whether every single number in the passed in parameter is even. If so, return the string true, otherwise return the string false. For example: if num is 4602225 your program should return the string false because 5 is not an even number.
+
+function SimpleEvens(num) {
+  let numArr = num.toString().split("");
+
+  for (let i = 0; i < numArr.length; i++) {
+    if (parseInt(numArr[i]) % 2 !== 0) {
+      return false;
+    }
+  }
+  return true;
+}
