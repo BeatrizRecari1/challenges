9eae8498b97b7752685c195a97809631311804a1
Beatriz Recari <beatrizrecari@gmail.com>
2023-05-31 17:23:15 +0100

    added second daily challenge

--- index.js
+++ index.js
@@ -1457,3 +1457,19 @@ function dashInsert(str) {
   }
   return result;
 }
+
+// Have the function OneDecremented(str) count how many times a digit appears that is exactly one less than the previous digit. For example: if str is "5655984" then your program should return 2 because 5 appears directly after 6 and 8 appears directly after 9. The input will always contain at least 1 digit.
+
+function oneDecremented(str) {
+  let result = 0;
+
+  for (let i = 1; i < str.length; i++) {
+    const previousNum = parseInt(str[i - 1]);
+    const currentNum = parseInt(str[i]);
+
+    if (currentNum === previousNum - 1) {
+      result++;
+    }
+  }
+  return result;
+}
