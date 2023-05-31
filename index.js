2fa88d05e18d6d645ef7afd738f739cd7358e426
Beatriz Recari <beatrizrecari@gmail.com>
2023-05-31 16:00:30 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -1442,3 +1442,18 @@ function SimpleEvens(num) {
   }
   return true;
 }
+
+// Have the function DashInsert(str) insert dashes ('-') between each two odd numbers in str. For example: if str is 454793 the output should be 4547-9-3. Don't count zero as an odd number.
+
+function dashInsert(str) {
+  let result = str[0];
+
+  for (let i = 1; i < srt.length; i++) {
+    if (str[i - 1] % 2 !== 0 && str[i] % 2 !== 0) {
+      result += "-" + str[i];
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
