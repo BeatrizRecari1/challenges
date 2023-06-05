6540161f566ccb11e96ce6817e5d6cb2156e8971
Beatriz Recari <beatrizrecari@gmail.com>
2023-06-05 17:05:56 -0400

    added second daily code challenge

--- index.js
+++ index.js
@@ -1505,7 +1505,20 @@ function changingSequence(arr) {
 }
 
 // Have the function BitwiseOne(strArr) take the array of strings stored in strArr, which will only contain two strings of equal length that represent binary numbers, and return a final binary string that performed the bitwise OR operation on both strings. A bitwise OR operation places a 0 in the new string where there are zeroes in both binary strings, otherwise it places a 1 in that spot. For example: if strArr is ["1001", "0100"] then your program should return the string "1101"
+function BitWiseOne(strArr) {
+  const binaryOne = strArr[0];
+  const binaryTwo = strArr[1];
+  let result = "";
 
+  for (let i = 0; i < binaryOne.length; i++) {
+    if (binaryOne[i] === "1" || binaryTwo[i] === "1") {
+      result += "1";
+    } else {
+      result += "0";
+    }
+  }
+  return result;
+}
 // Have the function WordCount(str) take the str string parameter being passed and return the number of words the string contains (e.g. "Never eat shredded wheat or cake" would return 6). Words will be separated by single spaces.
 
 function WordCount(str) {
