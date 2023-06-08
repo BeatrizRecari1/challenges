d1e23936b7324f6910e09b699f7b097b9da85962
Beatriz Recari <beatrizrecari@gmail.com>
2023-06-07 20:26:55 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -1615,3 +1615,17 @@ function RemoveBrackets(str) {
   }
   return count;
 }
+
+// Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it. For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+
+function FirstFactorial(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    let factorial = 1;
+    for (let i = 2; i <= num; i++) {
+      factorial *= i;
+    }
+  }
+  return factorial;
+}
