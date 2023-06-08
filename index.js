d8185754455155cd07a1c1493cb1908568a01fa7
Beatriz Recari <beatrizrecari@gmail.com>
2023-06-08 15:15:32 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -1629,3 +1629,21 @@ function FirstFactorial(num) {
   }
   return factorial;
 }
+
+// Have the function DistinctCharacters(str) take the str parameter being passed and determine if it contains at least 10 distinct characters, if so, then your program should return the string true, otherwise it should return the string false. For example: if str is "abc123kkmmmm?" then your program should return the string false because this string contains only 9 distinct characters: a, b, c, 1, 2, 3, k, m, ? adds up to 9.
+
+function DistinctCharacters(str) {
+  let newArray = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (!newArray.includes(char)) {
+      newArray.push(char);
+    }
+    if (newArray.length >= 10) {
+      return "true";
+    }
+  }
+  return "false";
+}
