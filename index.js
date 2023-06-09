d22e208fd749cbca73733178945bd72d79f7607b
Beatriz Recari <beatrizrecari@gmail.com>
2023-06-09 13:52:09 -0400

    added daily code challege

--- index.js
+++ index.js
@@ -1647,3 +1647,12 @@ function DistinctCharacters(str) {
   }
   return "false";
 }
+
+// Have the function TimeConvert(num) take the num parameter being passed and return the number of hours and minutes the parameter converts to (ie. if num = 63 then the output should be 1:3). Separate the number of hours and minutes with a colon.
+
+function timeConvert(num) {
+  const hours = Math.floor(num / 60);
+  const minutes = num % 60;
+
+  return hours + ":" + minutes;
+}
