a6a93b72793a7d795f75b08634764357c2ac55e3
Beatriz Recari <beatrizrecari@gmail.com>
2023-06-14 15:22:30 -0700

    added daily code challenge

--- index.js
+++ index.js
@@ -1797,3 +1797,27 @@ function AlphabetSoup(str) {
 String.prototype.toUpperCase = function () {
   return !/[a-z]/.test(this);
 };
+
+// Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+const languages = {
+  english: "Welcome",
+  czech: "Vitejte",
+  danish: "Velkomst",
+  dutch: "Welkom",
+  estonian: "Tere tulemast",
+  finnish: "Tervetuloa",
+  flemish: "Welgekomen",
+  french: "Bienvenue",
+  german: "Willkommen",
+  irish: "Failte",
+  italian: "Benvenuto",
+  latvian: "Gaidits",
+  lithuanian: "Laukiamas",
+  polish: "Witamy",
+  spanish: "Bienvenido",
+  swedish: "Valkommen",
+  welsh: "Croeso",
+};
+function Greeting(language) {
+  return languages[language] || "Welcome";
+}
