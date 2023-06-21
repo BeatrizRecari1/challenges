bf85128456ade86cd988989210f94ce67721f5cf
Beatriz Recari <beatrizrecari@gmail.com>
2023-06-21 10:35:36 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -1890,3 +1890,13 @@ function reversedArr(number) {
 
   return reversedDigits;
 }
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "False";
+  }
+}
