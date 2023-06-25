e7dfde051d25ce5303de3e98ff13a683adbcd02b
Beatriz Recari <beatrizrecari@gmail.com>
2023-06-25 14:48:48 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -1916,3 +1916,12 @@ function removeEveryOther(arr) {
   }
   return newArray;
 }
+
+// As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+function gimme(a) {
+  return a.indexOf(
+    a.concat().sort(function (a, b) {
+      return a - b;
+    })[1]
+  );
+}
