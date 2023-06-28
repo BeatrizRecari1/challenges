f89c732b1ed2a995e9dc9ee61f476ea739566089
Beatriz Recari <beatrizrecari@gmail.com>
2023-06-28 15:19:02 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -1925,3 +1925,11 @@ function gimme(a) {
     })[1]
   );
 }
+
+// Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+var isAnagram = function (test, original) {
+  var t = test.toLowerCase().split("").sort().join("");
+  var o = original.toLowerCase().split("").sort().join("");
+  return t == o ? true : false;
+};
