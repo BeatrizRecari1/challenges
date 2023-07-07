e02e2a1fa7c898baa9f7b2996607acdbc4adaca1
Beatriz Recari <beatrizrecari@gmail.com>
2023-07-07 09:28:03 -0400

    fixed code error

--- index.js
+++ index.js
@@ -1955,10 +1955,12 @@ function minMax(arr) {
 
 // Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
 
-function returnSum(a, b) {
-  if (a === b) {
-    return a;
+function GetSum(a, b) {
+  var result = 0;
+  var bigger = a > b ? a : b;
+  var smaller = a > b ? b : a;
+  for (var i = smaller; i <= bigger; i++) {
+    result += i;
   }
-
-  return a + b;
+  return result;
 }
