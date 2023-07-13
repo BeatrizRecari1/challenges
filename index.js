0fc94de16db91f11aa630b4d4649eb87a49b7c84
Beatriz Recari <beatrizrecari@gmail.com>
2023-07-13 11:34:25 +0100

    added second daily code challenge

--- index.js
+++ index.js
@@ -1978,3 +1978,47 @@ var championsLeagueGoals = 10;
 var copaDelReyGoals = 5;
 
 var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+
+// When provided with a number between 0-9, return it in words.
+
+function switchItUp(number) {
+  //Write your own Code!
+  switch (number) {
+    case 1:
+      1;
+      return "One";
+      break;
+    case 2:
+      2;
+      return "Two";
+      break;
+    case 3:
+      3;
+      return "Three";
+      break;
+    case 4:
+      4;
+      return "Four";
+      break;
+    case 5:
+      5;
+      return "Five";
+      break;
+    case 6:
+      6;
+      return "Six";
+      break;
+    case 7:
+      7;
+      return "Seven";
+      break;
+    case 8:
+      8;
+      return "Eight";
+      break;
+    case 9:
+      9;
+      return "Nine";
+      break;
+  }
+}
