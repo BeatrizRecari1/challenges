f4de78f7521d259521632db3349016ceffffb5bf
Beatriz Recari <beatrizrecari@gmail.com>
2023-07-13 11:29:48 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -1970,3 +1970,11 @@ function GetSum(a, b) {
 function filterList(l) {
   return l.filter((element) => typeof element === "number");
 }
+
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
