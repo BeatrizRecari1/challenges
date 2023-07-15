1d3cc4b009d9722fe31588a2ea9c31abb4367fab
Beatriz Recari <beatrizrecari@gmail.com>
2023-07-15 14:38:04 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -2033,3 +2033,16 @@ function move(position, roll) {
   // return the new position
   return position + roll * 2;
 }
+
+// Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+// The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year the new sum is re-invested.
+
+function calculateYears(principal, interest, tax, desired) {
+  // your code
+  var years = 0;
+  while (principal < desired) {
+    principal += principal * interest * (1 - tax);
+    years++;
+  }
+  return years;
+}
