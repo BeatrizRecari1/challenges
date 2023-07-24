ea6340b2c76daf1ba073593127f1eb80e2781424
Beatriz Recari <beatrizrecari@gmail.com>
2023-07-24 19:42:43 +0100

    added second daily challenge

--- index.js
+++ index.js
@@ -2098,3 +2098,17 @@ function ABCheck(str) {
   // then return false by default
   return false;
 }
+
+// Have the function ExOh(str) take the str parameter being passed and return the string true if there is an equal number of x's and o's, otherwise return the string false. Only these two letters will be entered in the string, no punctuation or numbers. For example: if str is "xooxxxxooxo" then the output should return false because there are 6 x's and 5 o's.
+function ExOh(str) {
+  str = str.toLowerCase();
+  var x = 0,
+    o = 0;
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] == "x") x++;
+    else if (str[i] == "o") o++;
+  }
+
+  // code goes here
+  return x == o;
+}
