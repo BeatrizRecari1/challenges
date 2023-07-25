e6359071c822bf864e3797a7206047ca8ce50cbf
Beatriz Recari <beatrizrecari@gmail.com>
2023-07-25 10:21:38 +0100

    fixed a code error on the last challenge

--- index.js
+++ index.js
@@ -2166,13 +2166,14 @@ function CountingMinutesI(str) {
 // Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example "Hello world123 567"
 
 function LongestWord(sen) {
-  sen = sen.replace(/[^a-zA-Z ]/gi, "");
-  words = sen.split(" ");
-  LongestWord = words[0];
-  for (i = 0; i < words.length; i++) {
-    if (words[i].length > LongestWord.length) {
-      LongestWord = words[i];
-    }
-  }
-  return LongestWord;
+  sen = sen.trim();
+  sen = sen.replace(/[^a-zA-Z0-9 ]/g, "");
+
+  let longest = "";
+
+  var arr = sen.split(" ").forEach((word) => {
+    if (word.length > longest.length) longest = word;
+  });
+
+  return longest;
 }
