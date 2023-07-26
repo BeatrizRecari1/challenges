624d149bbd1673a95950d93f451804e86364e573
Beatriz Recari <beatrizrecari@gmail.com>
2023-07-26 20:45:50 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -2177,3 +2177,16 @@ function LongestWord(sen) {
 
   return longest;
 }
+
+// program to find the factorial of a number using recursion
+function factorial(x) {
+  // if number is 0
+  if (x == 0) {
+    return 1;
+  }
+
+  // if number is positive
+  else {
+    return x * factorial(x - 1);
+  }
+}
