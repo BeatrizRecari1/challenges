036b305667be9618381f5fe2975f1c9fd8450185
Beatriz Recari <beatrizrecari@gmail.com>
2023-07-31 15:48:38 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -2237,3 +2237,14 @@ function doubleChar(str) {
   }
   return newString;
 }
+
+// Create a function mean that takes an array of numbers and returns the average of those numbers
+
+function mean(arr) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+
+  return result / arr.length;
+}
