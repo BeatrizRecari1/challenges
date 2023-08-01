74f1c65b606613e4e084a31c8e4ac80b9e24f774
Beatriz Recari <beatrizrecari@gmail.com>
2023-08-01 17:35:40 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -2287,3 +2287,9 @@ function checkExam(array1, array2) {
   }
   return result;
 }
+
+// Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+function removeChar(str) {
+  return str.substring(1, str.length - 1);
+}
