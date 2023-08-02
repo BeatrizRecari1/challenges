536f65c9e1672b8a5c3ad9568ee6b92f042c411f
Beatriz Recari <beatrizrecari@gmail.com>
2023-08-02 15:38:47 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -2310,3 +2310,14 @@ function stray(numbers) {
   }
   return sortedArray[sortedArray.length - 1];
 }
+
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(number) {
+  // your code........
+  if (number % 2 === 0) {
+    return number * 8;
+  } else if (number % 2 !== 0) {
+    return number * 9;
+  }
+}
