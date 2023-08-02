2700b4218538bdeef66b30dcdd28d167b81f7c52
Beatriz Recari <beatrizrecari@gmail.com>
2023-08-02 16:44:10 +0100

    added fifth daily code challenge

--- index.js
+++ index.js
@@ -2358,3 +2358,12 @@ function sum(numbers) {
 function noSpace(x) {
   return x.split(" ").join("");
 }
+
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+function fakeBin(x) {
+  return x
+    .split("")
+    .map((n) => (n < 5 ? 0 : 1))
+    .join("");
+}
