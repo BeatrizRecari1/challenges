fd3585b5bfe894c04e933c2cc9f161c26041a185
Beatriz Recari <beatrizrecari@gmail.com>
2023-08-02 15:55:13 +0100

    added third code challenge

--- index.js
+++ index.js
@@ -2335,3 +2335,20 @@ function updateLight(current) {
     return "green";
   }
 }
+
+// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+function sum(numbers) {
+  "use strict";
+  if (numbers === 0) {
+    return 0;
+  }
+
+  let sum = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+
+  return sum;
+}
