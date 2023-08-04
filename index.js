58f2f567c6bf868b7e7f3038ac2ee74aad2fa8fa
Beatriz Recari <beatrizrecari@gmail.com>
2023-08-04 15:34:49 +0100

    added another code challenge

--- index.js
+++ index.js
@@ -2433,3 +2433,20 @@ function factorial(n) {
   if (n < 0 || n > 12) throw new RangeError();
   return n <= 1 ? 1 : n * factorial(n - 1);
 }
+
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+// Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+// Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+function openOrSenior(data) {
+  // ...
+  let result = [];
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][0] >= 55 && data[i][1] > 7) {
+      result.push("Senior");
+    } else {
+      result.push("Open");
+    }
+  }
+  return result;
+}
