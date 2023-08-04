33b9b30c8de268ee3dc0c535d4e45427834065ef
Beatriz Recari <beatrizrecari@gmail.com>
2023-08-04 13:06:38 +0100

    added second daily code challenge

--- index.js
+++ index.js
@@ -2397,3 +2397,12 @@ const peopleWithAgeDrink = (age) =>
     : age < 21
     ? "drink beer"
     : "drink whisky";
+
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+function isTriangle(a, b, c) {
+  if (a + b > c && a + c > b && c + b > a) {
+    return true;
+  }
+  return false;
+}
