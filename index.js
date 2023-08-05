935119412a2b340c187f44a4bfe546f0e1387952
Beatriz Recari <beatrizrecari@gmail.com>
2023-08-05 18:02:02 +0100

    added another daily code challenge

--- index.js
+++ index.js
@@ -2528,3 +2528,26 @@ function sortOddNumbers(arr) {
 
   return resultArray;
 }
+
+// I have a cat and a dog.
+// I got them at the same time as kitten/puppy. That was humanYears years ago.
+// Return their respective ages now as [humanYears,catYears,dogYears]
+
+var humanYearsCatYearsDogYears = function (humanYears) {
+  // Your code here!
+  let catYears = 0;
+  let dogYears = 0;
+
+  if (humanYears === 1) {
+    catYears = 15;
+    dogYears = 15;
+  } else if (humanYears === 2) {
+    catYears = 24;
+    dogYears = 24;
+  } else if (humanYears >= 3) {
+    catYears = 24 + (humanYears - 2) * 4;
+    dogYears = 24 + (humanYears - 2) * 5;
+  }
+
+  return [humanYears, catYears, dogYears];
+};
