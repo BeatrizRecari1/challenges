b3c629d7c8c9a6bc23342261104b23937b7b7e89
Beatriz Recari <beatrizrecari@gmail.com>
2023-08-06 14:04:40 +0100

    added second daily code challenge

--- index.js
+++ index.js
@@ -2561,3 +2561,32 @@ function countSheep(num) {
   }
   return result;
 }
+
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+function likes(names) {
+  names = names || [];
+  switch (names.length) {
+    case 0:
+      return "no one likes this";
+      break;
+    case 1:
+      return names[0] + " likes this";
+      break;
+    case 2:
+      return names[0] + " and " + names[1] + " like this";
+      break;
+    case 3:
+      return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+      break;
+    default:
+      return (
+        names[0] +
+        ", " +
+        names[1] +
+        " and " +
+        (names.length - 2) +
+        " others like this"
+      );
+  }
+}
