66fbc9055bd5d6df3e13805fe5cb91145a893361
Beatriz Recari <beatrizrecari@gmail.com>
2023-08-06 13:52:05 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -2551,3 +2551,13 @@ var humanYearsCatYearsDogYears = function (humanYears) {
 
   return [humanYears, catYears, dogYears];
 };
+
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+function countSheep(num) {
+  let result = "";
+  for (let i = 0; i <= num; i++) {
+    result += i + " sheep...";
+  }
+  return result;
+}
