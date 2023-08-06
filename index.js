dac5df4c267e57858bb4f64b293522515cf58179
Beatriz Recari <beatrizrecari@gmail.com>
2023-08-06 14:30:24 +0100

    added another code challenge

--- index.js
+++ index.js
@@ -2590,3 +2590,16 @@ function likes(names) {
       );
   }
 }
+
+// You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+
+function findEvenIndex(arr) {
+  let left = 0;
+  let right = arr.reduce((s, n) => s + n, 0);
+  for (let i = 0; i < arr.length; i++) {
+    right -= arr[i];
+    if (left === right) return i;
+    left += arr[i];
+  }
+  return -1;
+}
