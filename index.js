33b5a6762d15f8c12f242f4651d1eefc8df5b38e
Beatriz Recari <beatrizrecari@gmail.com>
2023-08-08 21:26:32 +0200

    added daily code challenge

--- index.js
+++ index.js
@@ -2613,3 +2613,20 @@ function sumMix(x) {
   }
   return sum;
 }
+
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+function oddOrEven(array) {
+  //enter code here
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  if (sum % 2 === 0) {
+    return "even";
+  } else if (sum === [0]) {
+    return "even";
+  } else {
+    return "odd";
+  }
+}
