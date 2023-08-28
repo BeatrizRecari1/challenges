afd8209ebfef5b4f099fe5dc46c455e6de9ba82f
Beatriz Recari <beatrizrecari@gmail.com>
2023-08-28 14:57:25 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -2707,3 +2707,18 @@ function duplicateCount(text) {
       .match(/([^])\1+/g) || []
   ).length;
 }
+
+// Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+function towerBuilder(nFloors) {
+  // build here
+  let space,
+    star,
+    tower = [];
+  for (i = 1; i <= nFloors; i++) {
+    space = " ".repeat(nFloors - i);
+    star = "*".repeat(2 * i - 1);
+    tower.push(`${space}${star}${space}`);
+  }
+  return tower;
+}
