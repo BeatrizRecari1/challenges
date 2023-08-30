bd4bb4a06db73b0809f641d156c826d54b2957e7
Beatriz Recari <beatrizrecari@gmail.com>
2023-08-30 11:24:18 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -2741,3 +2741,13 @@ function expandedForm(num) {
 
   return result; // Return the expanded form as a string.
 }
+
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+function paperwork(n, m) {
+  if (n < 0 || m < 0) {
+    return 0;
+  }
+  return n * m;
+}
