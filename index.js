a4122036ace2c523ba8299b9e1deb416b919703a
Beatriz Recari <beatrizrecari@gmail.com>
2023-08-31 12:46:22 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -2751,3 +2751,17 @@ function paperwork(n, m) {
   }
   return n * m;
 }
+
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+function findUniq(arr) {
+  // do magic
+  let firstElement = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (firstElement !== arr[1] && firstElement !== arr[2]) {
+      return firstElement;
+    } else if (firstElement !== arr[i]) {
+      return arr[i];
+    }
+  }
+}
