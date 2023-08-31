31c81363e44adeb1fa0d5b4e50dd086c352e69a1
Beatriz Recari <beatrizrecari@gmail.com>
2023-08-31 12:52:34 +0100

    added second daily code challenge

--- index.js
+++ index.js
@@ -2765,3 +2765,30 @@ function findUniq(arr) {
     }
   }
 }
+
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+function points(games) {
+  // totalPoints will store the accumulated points
+  let totalPoints = 0;
+
+  // loop through the games array to get the scores
+  for (i = 0; i < games.length; i++) {
+    // assign the scores, since games is an array of strings, we must use Number() to turn the string scores into numbers
+    let xScore = Number(games[i].charAt(0));
+    let yScore = Number(games[i].charAt(2));
+
+    // Add points depending on the values of xScore compared to yScore
+    if (xScore > yScore) {
+      totalPoints += 3;
+    }
+    if (xScore < yScore) {
+      totalPoints += 0;
+    }
+    if (xScore === yScore) {
+      totalPoints += 1;
+    }
+  }
+
+  return totalPoints;
+}
