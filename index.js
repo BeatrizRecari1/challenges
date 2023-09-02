0eabdab4b1bb4e57d7671a69417cebbdc212f433
Beatriz Recari <beatrizrecari@gmail.com>
2023-09-02 11:31:01 +0100

    added daily code challenge

--- index.js
+++ index.js
@@ -2792,3 +2792,9 @@ function points(games) {
 
   return totalPoints;
 }
+
+// Complete the solution so that it reverses the string passed into it.
+
+function solution(str) {
+  return str.split("").reverse().join("");
+}
