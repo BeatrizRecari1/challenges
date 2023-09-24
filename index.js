ee8eb92081d528fa2ca81a21d3d5281e37db0835
Beatriz Recari <beatrizrecari@gmail.com>
2023-09-24 10:50:09 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -2914,3 +2914,12 @@ function summation(num) {
   }
   return sum;
 }
+
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+function highAndLow(numbers) {
+  var arr = numbers.split(" ").sort(function (a, b) {
+    return a - b;
+  });
+  return arr[arr.length - 1] + " " + arr[0];
+}
