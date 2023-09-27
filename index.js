c9bc050b14243f267e6e4ea0ee791931f9b645a3
Beatriz Recari <beatrizrecari@gmail.com>
2023-09-26 20:19:45 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -2930,3 +2930,18 @@ function highAndLow(numbers) {
   numbers = numbers.split(" ");
   return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
 }
+
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+function order(words) {
+  var array = words.split(" ");
+  var sortedArray = [];
+  for (i = 0; i <= array.length; i++) {
+    for (j = 0; j < array.length; j++) {
+      if (array[j].indexOf(i) >= 0) {
+        sortedArray.push(array[j]);
+      }
+    }
+  }
+  return sortedArray.join(" ");
+}
