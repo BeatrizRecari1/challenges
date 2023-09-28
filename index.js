b532944926cf228597367390499ccfb8f373589c
Beatriz Recari <beatrizrecari@gmail.com>
2023-09-27 20:38:45 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -2945,3 +2945,13 @@ function order(words) {
   }
   return sortedArray.join(" ");
 }
+
+// Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+String.prototype.toJadenCase = function () {
+  return this.split(" ")
+    .map(function (word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ");
+};
