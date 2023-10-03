68745c063041d6752e0a156335d4014640eb1c88
Beatriz Recari <beatrizrecari@gmail.com>
2023-10-03 19:01:14 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -2994,3 +2994,16 @@ function deleteNth(arr, x) {
     return cache[n] <= x;
   });
 }
+
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+function longest(s1, s2) {
+  // your code
+  // Combine the two strings and remove duplicates
+  const combined = Array.from(new Set(s1 + s2));
+
+  // Sort the combined string
+  const sorted = combined.sort().join("");
+
+  return sorted;
+}
