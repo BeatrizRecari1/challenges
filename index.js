9d2267e7afd729c3dfd12b1d7494f683eadc4082
Beatriz Recari <beatrizrecari@gmail.com>
2023-10-02 20:21:27 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -2985,3 +2985,12 @@ function strCount(str, letter) {
   }
   return count;
 }
+
+// Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+function deleteNth(arr, x) {
+  var cache = {};
+  return arr.filter(function (n) {
+    cache[n] = (cache[n] || 0) + 1;
+    return cache[n] <= x;
+  });
+}
