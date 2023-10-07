89401562daa0546430d5dffaef467ee71a083bb0
Beatriz Recari <beatrizrecari@gmail.com>
2023-10-07 19:51:05 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -3024,3 +3024,11 @@ function count(string) {
   });
   return count;
 }
+
+// Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+function isPangram(string) {
+  return "abcdefghijklmnopqrstuvwxyz"
+    .split("")
+    .every((x) => string.toLowerCase().includes(x));
+}
