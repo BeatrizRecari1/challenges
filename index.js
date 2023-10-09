ee24f344c518ba437aaca483327648ef9d164123
Beatriz Recari <beatrizrecari@gmail.com>
2023-10-09 09:30:19 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -3032,3 +3032,13 @@ function isPangram(string) {
     .split("")
     .every((x) => string.toLowerCase().includes(x));
 }
+
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+function twoSum(numbers, target) {
+  for (var i = 0; i < numbers.length - 1; i++) {
+    for (var j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) return [i, j];
+    }
+  }
+}
