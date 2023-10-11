66ae16d9c968b186e69259d2522bb5fd7011912e
Beatriz Recari <beatrizrecari@gmail.com>
2023-10-10 20:06:17 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -3042,3 +3042,14 @@ function twoSum(numbers, target) {
     }
   }
 }
+
+// Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+function sumDigits(number) {
+  return Math.abs(number)
+    .toString()
+    .split("")
+    .reduce(function (a, b) {
+      return +a + +b;
+    }, 0);
+}
