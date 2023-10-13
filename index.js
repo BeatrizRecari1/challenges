ce963db11817163bf4bfa0b1a01877a8e4af78a5
Beatriz Recari <beatrizrecari@gmail.com>
2023-10-13 10:16:39 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -3080,3 +3080,13 @@ function getDivisorsCnt(n) {
   }
   return num;
 }
+
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+function problem(x) {
+  //your code here
+  if (typeof x === "string") {
+    return "Error";
+  }
+  return x * 50 + 6;
+}
