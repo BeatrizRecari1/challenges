36737a60b64349abc82e5c05448ef23a019985bf
Beatriz Recari <beatrizrecari@gmail.com>
2023-10-18 14:12:34 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -3114,3 +3114,17 @@ function xor(a, b) {
     return false;
   }
 }
+
+// Create a function that returns the name of the winner in a fight between two fighters.
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  var fac1 = Math.ceil(fighter1.health / fighter2.damagePerAttack);
+  var fac2 = Math.ceil(fighter2.health / fighter1.damagePerAttack);
+  if (fac1 < fac2) {
+    return fighter2.name;
+  } else if (fac2 < fac1) {
+    return fighter1.name;
+  } else {
+    return firstAttacker;
+  }
+}
