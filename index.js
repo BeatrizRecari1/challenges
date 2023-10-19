dbb07a1e3f5ef8cd3d0c4a0ee5c59e14fd48451c
Beatriz Recari <beatrizrecari@gmail.com>
2023-10-19 13:54:52 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -3128,3 +3128,16 @@ function declareWinner(fighter1, fighter2, firstAttacker) {
     return firstAttacker;
   }
 }
+
+// Given an array of integers your solution should find the smallest integer.
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    let smallestInt = args[0];
+    for (let i = 0; i < args.length; i++) {
+      if (args[i] < smallestInt) {
+        smallestInt = args[i];
+      }
+    }
+    return smallestInt;
+  }
+}
