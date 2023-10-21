0d4c95eca5ff7a5fb64862ab31e0637fadb05a00
Beatriz Recari <beatrizrecari@gmail.com>
2023-10-20 20:29:17 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -3141,3 +3141,16 @@ class SmallestIntegerFinder {
     return smallestInt;
   }
 }
+
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+function between(a, b) {
+  // your code here
+  let result = [];
+
+  for (let i = a; i <= b; i++) {
+    result.push(i);
+  }
+
+  return result;
+}
