152256a2e3d2f7dd3a4c23d34549fae2434a0e07
Beatriz Recari <beatrizrecari@gmail.com>
2023-10-21 19:20:10 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -3154,3 +3154,10 @@ function between(a, b) {
 
   return result;
 }
+
+// When provided with a letter, return its position in the alphabet.
+
+function position(letter) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  return "Position of alphabet: " + (alphabet.indexOf(letter) + 1);
+}
