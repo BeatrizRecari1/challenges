b38979c2e0cdfcc398a5b645b161520462711dd9
Beatriz Recari <beatrizrecari@gmail.com>
2023-10-22 16:57:19 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -3161,3 +3161,17 @@ function position(letter) {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
   return "Position of alphabet: " + (alphabet.indexOf(letter) + 1);
 }
+
+// The number 89 = 8 + 9^2
+
+function sumDigPow(a, b) {
+  var arr = [];
+  for (var i = a; i <= b; i++) {
+    var sum = 0;
+    for (var j = 0; j <= String(i).length; j++) {
+      sum += Math.pow(parseInt(String(i)[j]), j + 1);
+      if (sum == i) arr.push(i);
+    }
+  }
+  return arr;
+}
