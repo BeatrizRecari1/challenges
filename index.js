955bc6ef3d1ed6a6f8a3223c006ed83a06a7e40a
Beatriz Recari <beatrizrecari@gmail.com>
2023-10-30 13:12:54 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -3220,3 +3220,18 @@ function plural(n) {
     return true;
   }
 }
+
+// Daily challenge from Codewars
+
+function stockList(listOfArt, listOfCat) {
+  if (!listOfArt.length || !listOfCat.length) return "";
+  return listOfCat
+    .map((w) => {
+      const s = listOfArt.reduce(
+        (a, b) => a + (b.charAt(0) === w ? +b.split(" ")[1] : 0),
+        0
+      );
+      return `(${w} : ${s})`;
+    })
+    .join(" - ");
+}
