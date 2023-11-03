7fad66e12c89db8ba12e53f80a96231e2fe5f1a2
Beatriz Recari <beatrizrecari@gmail.com>
2023-11-02 20:48:15 -0400

    added daily code challenge

--- index.js
+++ index.js
@@ -3249,3 +3249,11 @@ function findNb(m) {
 
   return sum === m ? n - 1 : -1;
 }
+
+// Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+function take(arr, n) {
+  // Your code here
+
+  return arr.slice(0, n);
+}
