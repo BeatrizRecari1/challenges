3c6a2a52243a1b5a25993bf899a182ea9d0cf199
Beatriz Recari <beatrizrecari@gmail.com>
2023-11-21 22:08:58 -0500

    added daily code challenge

--- index.js
+++ index.js
@@ -3386,3 +3386,12 @@ function revrot(str, sz) {
 
   return chunks.join("");
 }
+
+// Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+function comp(array1, array2) {
+  if (array1 == null || array2 == null) return false;
+  array1.sort((a, b) => a - b);
+  array2.sort((a, b) => a - b);
+  return array1.map((v) => v * v).every((v, i) => v == array2[i]);
+}
